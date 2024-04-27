dca979cbe6c6c147f7e3c3a68167eeec6e177bab
ptd3v <tovey@live.co.uk>
2024-04-27 16:47:21 +0100

    return ' '.join(word.capitalize() for word in string.split())

--- jaden-case.py
+++ jaden-case.py
@@ -3,6 +3,15 @@
 # Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram.
 
 # Solution One
-
 def to_jaden_case(string):
     return ' '.join(word.capitalize() for word in string.split())
+
+# Solution Two
+def to_jaden_case(string):
+    list = string.split()
+    new_list = [i.capitalize() for i in list]
+    return ' '.join(new_list)
+
+# Solution Three
+import string
+toJadenCase = string.capwords
