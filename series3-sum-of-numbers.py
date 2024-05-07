3d64d821726df5e5951f3b7e5bb5e4b11aaa8009
ptd3v <tovey@live.co.uk>
2024-05-07 09:03:35 +0100

    series3-sum-of-numbers init

---
+++ series3-sum-of-numbers.py
@@ -0,0 +1,4 @@
+# Beginner Series #3 Sum of Numbers (7th Kyu)
+# Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it.
+
+# Solution One
