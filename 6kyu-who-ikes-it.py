329dd2bb97009feb889af504b9aa28cced3b3991
ptd3v <tovey@live.co.uk>
2024-05-14 12:09:02 +0100

    return f"{names[0]}, {names[1]} and {n - 2} others like this"

---
+++ 6kyu-who-ikes-it.py
@@ -0,0 +1,29 @@
+# Who Likes it? (6th Kyu)
+# Implement the function which takes an array containing the names of people that like an item.
+
+# Functions Required: 
+
+# My Solution:
+def likes(names):
+    n = len(names)
+    if n == 0:
+        return ("no one likes this")
+    elif n == 1:
+        return f"{names[0]} likes this"
+    elif n == 2:
+        return f"{names[0]} and {names[1]} like this"
+    elif n == 3:
+        return f"{names[0]}, {names[1]} and {names[2]} like this"
+    else:
+        return f"{names[0]}, {names[1]} and {n - 2} others like this"
+
+# Community Solution:
+def likes(names):
+    n = len(names)
+    return {
+        0: 'no one likes this',
+        1: '{} likes this', 
+        2: '{} and {} like this', 
+        3: '{}, {} and {} like this', 
+        4: '{}, {} and {others} others like this'
+    }[min(4, n)].format(*names[:3], others=n-2)
