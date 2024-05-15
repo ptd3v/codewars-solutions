23706986474c38ba8b31decfdd1b98f0c255ea3b
ptd3v <tovey@live.co.uk>
2024-05-15 14:43:31 +0100

    n = sum(int(x) for x in str(n))

--- 6kyu-sum-of-digits.py
+++ 6kyu-sum-of-digits.py
@@ -2,3 +2,11 @@
 # Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced.
 
 # My Solution:
+def digital_root(n):
+    while n > 9:
+        n = sum(int(x) for x in str(n))
+    return n
+
+# Community Solution
+def digital_root(n):
+	return n % 9 or n and 9
