e70c8fa2b9145348405e56c1272dbaa6ff26ca76
ptd3v <tovey@live.co.uk>
2024-05-15 10:05:55 +0100

    Sum of Digits (6th Kyu)

---
+++ 6kyu-sum-of-digits.py
@@ -0,0 +1,4 @@
+# Sum of Digits (6th Kyu)
+# Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced.
+
+# My Solution:
