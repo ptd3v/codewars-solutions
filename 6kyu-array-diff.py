7eec6019626bae470247527d7ac48d6698dbfac8
ptd3v <tovey@live.co.uk>
2024-05-19 19:50:32 +0100

    return [x for x in a if x not in b]

--- 6kyu-array-diff.py
+++ 6kyu-array-diff.py
@@ -2,3 +2,13 @@
 # Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
 
 # My Solution:
+def array_diff(a, b):
+    ends = []
+    for x in a:
+        if x not in b:
+            ends.append(x)
+    return ends
+
+# Community Solution:
+def array_diff(a, b):
+    return [x for x in a if x not in b]
