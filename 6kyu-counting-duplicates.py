fae2a8c4d43cf327493662d6f3c3203ad80ec61a
ptd3v <tovey@live.co.uk>
2024-05-23 23:23:51 +0100

    File init

---
+++ 6kyu-counting-duplicates.py
@@ -0,0 +1,4 @@
+# 6 kyu Counting Duplicates
+# Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. 
+
+# My Solution:
