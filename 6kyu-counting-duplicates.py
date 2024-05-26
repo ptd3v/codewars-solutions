442ecbffc2183ae306124cefd0d50bd6e1e2037f
ptd3v <102877038+ptd3v@users.noreply.github.com>
2024-05-26 15:37:15 +0100

    return len([c for c in set(text.lower()) if text.lower().count(c)>1])

--- 6kyu-counting-duplicates.py
+++ 6kyu-counting-duplicates.py
@@ -2,3 +2,12 @@
 # Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. 
 
 # My Solution:
+def duplicate_count(text):
+    result = []
+    for c in set(text.lower()):
+        if text.lower().count(c) > 1:
+            result.append(text)
+    return len(result)
+
+# Community Solution
+return len([c for c in set(text.lower()) if text.lower().count(c)>1])
