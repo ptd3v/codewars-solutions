81e81c1c8758f1fdf3760e8e73f72243f53ce3e8
ptd3v <102877038+ptd3v@users.noreply.github.com>
2024-07-19 16:59:32 +0100

    if step == 'n': y += 1

---
+++ 6kyu-take-10-minute-walk.py
@@ -0,0 +1,23 @@
+# Take a Ten Minutes Walk (6th Kyu)
+# When you press a button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e'])
+
+# My Solution:
+def is_valid_walk(walk):
+    if len(walk) != 10:
+        return False
+   
+    x, y = 0, 0   
+    for step in walk:
+        if step == 'n':
+            y += 1
+        elif step == 's':
+            y -=1
+        elif step == 'e':
+            x -= 1
+        elif step == 'w':
+            x += 1       
+    return x == 0 and y == 0
+
+# Community Solution:
+return len(walk) == 10 and walk.count('n') == walk.count('s') and walk.count('e') == walk.count('w')
+
