9a11c34dbc0ed6aa4fb1aa9becf28aa6df2e9b94
ptd3v <102877038+ptd3v@users.noreply.github.com>
2024-07-21 15:18:05 +0100

    if (walk.count('n') == walk.count('s') and

--- 6kyu-take-10-minute-walk.py
+++ 6kyu-take-10-minute-walk.py
@@ -21,3 +21,11 @@ def is_valid_walk(walk):
 # Community Solution:
 return len(walk) == 10 and walk.count('n') == walk.count('s') and walk.count('e') == walk.count('w')
 
+# Longer Form
+def isValidWalk(walk):
+    if (walk.count('n') == walk.count('s') and 
+        walk.count('e') == walk.count('w') and
+        len(walk) == 10):
+            return True
+    return False
+
