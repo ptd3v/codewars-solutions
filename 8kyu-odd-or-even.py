6ea0e91746c24003adbd00bf5474a58257d05a45
ptd3v <102877038+ptd3v@users.noreply.github.com>
2024-08-27 20:35:50 +0100

    return 'Odd' if number % 2 else 'Even'

---
+++ 8kyu-odd-or-even.py
@@ -0,0 +1,11 @@
+# Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+def even_or_odd(number):
+    if number % 2 == 0:
+        return "Even"
+    return "Odd"
+
+# Community Answer:
+def even_or_odd(number):
+	return 'Odd' if number % 2 else 'Even'
+
