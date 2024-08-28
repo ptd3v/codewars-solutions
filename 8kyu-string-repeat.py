1c3dd2f0487e3c8461f51265f06d6f3005c71a2c
ptd3v <102877038+ptd3v@users.noreply.github.com>
2024-08-28 18:48:15 +0100

    return string * repeat

---
+++ 8kyu-string-repeat.py
@@ -0,0 +1,9 @@
+# 8 kyu: String repeat
+# Return a string of s repeated exactly n times.
+
+# My Solution:
+def repeat_str(repeat, string):
+    return string * repeat
+
+# Community Solution
+repeat_str = lambda r, s: s * r
