dc1021c13282896ead31255e43f6bcd278d723e2
ptd3v <102877038+ptd3v@users.noreply.github.com>
2024-08-28 17:46:47 +0100

    return str(num)

---
+++ 8kyu-convery-int-to-string.py
@@ -0,0 +1,10 @@
+# 8 Kyu: Convert a Number to a String!
+# We need a function that can transform a number (integer) into a string.
+
+# My Solution:
+def number_to_string(num):
+    return str(num)
+
+# Community Solution
+def number_to_string(num):
+    ## Hilarious answers, none useful.
