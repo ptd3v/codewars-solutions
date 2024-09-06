5719d9e1ed1e6023170c26d521a18febeee7be3f
ptd3v <tovey@live.co.uk>
2024-09-06 19:56:57 +0100

    return [sort[0], + sort[-1]]

---
+++ 7Kyu-highest-profit-wins.py
@@ -0,0 +1,9 @@
+# 7Kyu The highest profit wins!
+# Ben has a very simple idea to make some profit: he buys something and sells it again.
+
+# My Solution:
+def min_max(lst):
+    sort = sorted(lst)
+    return [sort[0], + sort[-1]]
+
+# Community Solution
---
+++ 7kyu-sum-the-first-nth.py
@@ -0,0 +1,9 @@
+# 7 kyu: Sum of the first nth term of Series
+# Your task is to write a function which returns the n-th term of the following series.
+
+# My Solution:
+
+
+# Community Solution
+
+
