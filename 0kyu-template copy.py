72c52266214dc571140bf9dc00a8d47fc2c88014
ptd3v <tovey@live.co.uk>
2024-09-19 14:23:11 +0100

    product *= int(digit)

---
+++ 0kyu-template copy.py
@@ -0,0 +1,10 @@
+# Title
+# 
+# 
+
+# My Solution:
+
+
+# Community Solution
+
+
--- 6kyu-persistent-bugger.py
+++ 6kyu-persistent-bugger.py
@@ -2,6 +2,24 @@
 # Take in a positive parameter num and return its multiplicative persistence.
 
 # My Solution
-
+def persistence(n):
+    count = 0
+    while n >= 10:
+        product = 1
+        for digit in str(n):
+            product *= int(digit)
+        n = product
+        count += 1
+    return count
 
 # Community Solution
+def persistence(n):
+    n = str(n)
+    count = 0
+    while len(n) > 1:
+        p = 1
+        for i in n:
+            p *= int(i)
+        n = str(p)
+        count += 1
+    return count
